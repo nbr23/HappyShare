c05a8c4cbda6227b1df13a40e8c00cf9f49344e5
Maxence <max@23.tf>
2020-02-03 21:54:13 -0500

    Add notification display for upload status

--- app/src/main/java/fr/catch23/happyshare/ShareToApi.kt
+++ app/src/main/java/fr/catch23/happyshare/ShareToApi.kt
@@ -1,14 +1,33 @@
 package fr.catch23.happyshare
 
 import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Toast
 
 class ShareToApi : Activity() {
     private var mHandler: Handler? = null
+
+
+
+    private fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = context.getString(R.string.notification_channel_name)
+            val descriptionText = context.getString(R.string.notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(getString(R.string.notification_channel_id), name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +38,12 @@ class ShareToApi : Activity() {
         mHandler = Handler()
         val activity = this
 
+        createNotificationChannel(context)
+
         if (Intent.ACTION_SEND == action && type != null) {
             val apimger = APIManager(context, mHandler!!)
+
+
             val thread = object : Thread() {
                 override fun run() {
                     try {
